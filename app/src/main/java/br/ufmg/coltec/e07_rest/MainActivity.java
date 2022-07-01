afdfbf645d859f07751a6e77840b5e40eae0fa67
rafaalt <rafaaslat@gmail.com>
2022-06-30 21:44:56 -0300

    Entrega Atividade

--- app/src/main/java/br/ufmg/coltec/e07_rest/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/e07_rest/MainActivity.java
@@ -2,7 +2,14 @@ package br.ufmg.coltec.e07_rest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnProcurar = findViewById(R.id.btnBuscar);
+        EditText editText = findViewById(R.id.txtUsuario);
+
+        btnProcurar.setOnClickListener(view -> {
+            String usuario = editText.getText().toString();
+
+            RetrofitConfig retrofitConfig = RetrofitConfig.getInstance();
+            GitService gitService = retrofitConfig.getGitService();
+
+            Call<User> call = gitService.getUser(usuario);
+
+            call.enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    User user = response.body();
+                    Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+
+                }
+            });
+        });
     }
 }
